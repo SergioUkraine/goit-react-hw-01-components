dd95087e710b723ef4274bbd738412ea0e84e241
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-07-05 22:32:16 +0300

    profile & statistcs done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
+import user from '../data/user.json';
+import stats from '../data/data.json';
+import css from './App.module.css';
+
+const title = 'Some title';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <div className={css.profile}>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        <Statistics stats={stats} title={title} />
+      </div>
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,42 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css['stat-list']}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
